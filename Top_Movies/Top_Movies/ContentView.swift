7edc5e23fd7f641a03e0df4e0ff0d07c6f3e5437
a.gorjipoor <a.gorjipoor@nizek.com>
2025-03-09 23:05:09 +0330

    project setup

---
+++ Top_Movies/Top_Movies/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  Top_Movies
+//
+//  Created by Arman on 3/9/25.
+//
+
+import SwiftUI
+import SwiftData
+
+struct ContentView: View {
+    var body: some View {
+      Text("Select an item")
+        }
+    }
+
+#Preview {
+    
+}
---
+++ Top_Movies/Top_Movies/Top_MoviesApp.swift
@@ -0,0 +1,32 @@
+//
+//  Top_MoviesApp.swift
+//  Top_Movies
+//
+//  Created by Arman on 3/9/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Top_MoviesApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
